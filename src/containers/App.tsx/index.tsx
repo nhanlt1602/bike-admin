3ce2b8326afc749c05ce6b1d957bddd9056a4eab
nhanlt1602 <nhanlt16235@gmail.com>
2021-09-28 11:21:52 +0700

    [NhanLT] Symptom management load success

--- src/containers/App.tsx/index.tsx
+++ src/containers/App.tsx/index.tsx
@@ -1,11 +1,9 @@
-import Hospitals from "../HospitalManagement";
 import Layout from "../Layout";
 import Symptoms from "../SymptomManagement";
 
 const App = () => {
     return (
         <Layout>
-            <Hospitals />
             <Symptoms />
         </Layout>
     );
