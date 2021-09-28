a25736538db025c549436f21cb060da150be8e78
binhtt131222000 <binhtt.13122000@gmail.com>
2021-09-28 12:24:10 +0700

    BINHTT: fix conflict

--- src/containers/App.tsx/index.tsx
+++ src/containers/App.tsx/index.tsx
@@ -4,16 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import RouteComponent from "src/routes";
 
 import Layout from "../Layout";
-import Symptoms from "../SymptomManagement";
 
 import SnackbarProvider from "src/context/SnackbarProvider.context";
 
 const App = () => {
     return (
         <React.Fragment>
-            <Layout>
-                <Symptoms />
-            </Layout>
             <BrowserRouter>
                 <SnackbarProvider>
                     <Layout>
