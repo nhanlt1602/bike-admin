00499163da10025570e4b230237a876eb54f906d
chloe-nguyen <chloenguyen1206@gmail.com>
2021-10-03 16:06:46 +0700

    add font, FB icon, bgr img, fb btn component

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import FacebookButton from "src/components/Button/FacebookButton";
 import GoogleButton from "src/components/Button/GoogleButton";
 
 import loginImg from "../../assets/login.jpg";
 
-import { Grid, Box } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import useAuth, { facebookProvider, googleProvider } from "src/hooks/useAuth";
 
 const Login: React.FC = () => {
@@ -21,25 +23,45 @@ const Login: React.FC = () => {
                 justifyContent="center"
                 alignItems="center"
             >
-                <Grid item xs={6} sx={{ display: "flex", flexDirection: "row" }}>
-                    <Box>
-                        <GoogleButton
-                            variant="outlined"
-                            onClick={() => login(googleProvider)}
-                            fullWidth
-                        >
-                            Google
-                        </GoogleButton>
-                    </Box>
-                    <Box>
-                        <GoogleButton
-                            variant="outlined"
-                            onClick={() => login(facebookProvider)}
-                            fullWidth
-                        >
-                            Facebook
-                        </GoogleButton>
-                    </Box>
+                <Grid item xs={6} pt="6">
+                    <Grid xs={12} sx={{ display: "flex", justifyContent: "center" }}>
+                        <span style={{ fontFamily: "Philosopher", fontSize: 100 }}>
+                            Telemedicine
+                        </span>
+                    </Grid>
+                    <Grid xs={12} sx={{ display: "flex", justifyContent: "center" }}>
+                        <Typography variant="overline" fontSize="20px">
+                            Bắt đầu ứng dụng với
+                        </Typography>
+                    </Grid>
+                    <Grid
+                        xs={12}
+                        sx={{
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "center",
+                            m: 3,
+                        }}
+                    >
+                        <Box sx={{ width: 140, pr: 3 }}>
+                            <GoogleButton
+                                variant="outlined"
+                                fullWidth
+                                onClick={() => login(googleProvider)}
+                            >
+                                Google
+                            </GoogleButton>
+                        </Box>
+                        <Box sx={{ width: 140 }}>
+                            <FacebookButton
+                                variant="outlined"
+                                fullWidth
+                                onClick={() => login(facebookProvider)}
+                            >
+                                Facebook
+                            </FacebookButton>
+                        </Box>
+                    </Grid>
                 </Grid>
                 <Grid item xs={6}></Grid>
             </Grid>
