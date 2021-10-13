7361dba256d1324c29b7aeae3873b17ced13d634
chloe-nguyen <chloenguyen1206@gmail.com>
2021-10-13 08:10:54 +0700

    NgocNTH: change icon into img icon and app logo

--- src/components/ListRoutes/components/RoutesCollapse/index.tsx
+++ src/components/ListRoutes/components/RoutesCollapse/index.tsx
@@ -13,7 +13,7 @@ export type RoutesCollapseType = {
         id: number;
         name: string;
         path: string;
-        icon: JSX.Element;
+        icon: string;
     }[];
     handleListItemClick: (
         index: number,
@@ -48,7 +48,14 @@ const RoutesCollapse: React.FC<RoutesCollapseType> = (props: RoutesCollapseType)
                                 )
                             }
                         >
-                            <ListItemIcon>{itemChildren?.icon}</ListItemIcon>
+                            <ListItemIcon>
+                                <img
+                                    src={itemChildren?.icon}
+                                    width="30px"
+                                    height="auto"
+                                    alt="icon"
+                                />
+                            </ListItemIcon>
                             <ListItemText primary={itemChildren?.name} />
                         </ListItem>
                     );
